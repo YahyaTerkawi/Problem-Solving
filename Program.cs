0a448b0109c7890c6f5bb9a80ba80af0ff0dbc04
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-08 15:55:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿static class program
+{
+
+    static int ReverseNumber(int num)
+    {
+        int ReversedNumber = 0;
+        int Reminder = 0;
+
+        while (num > 0)
+        {
+            Reminder = num % 10;
+            num /= 10;
+            ReversedNumber = ReversedNumber * 10 + Reminder;
+            
+        }
+
+
+
+
+
+        return ReversedNumber;
+    
+    }
+
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ReverseNumber(999777));
+
+        Console.ReadKey();
+    }
+}
