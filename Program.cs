edb4df73f6bcb2c46593b86bc8a4e39631b2decf
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-08 15:32:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿static class program
+{
+
+    static void PrintHeader()
+    {
+        Console.WriteLine();
+        Console.WriteLine("\t\t\t Multiplication Table From 1 to 10");
+        Console.WriteLine();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"\t {i}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("________________________________________________________________________________________________________");
+    }
+
+    static void PrintMultiPlicationTableX(int Table , string ColumnSeperaor= "    ")
+    {
+        Console.WriteLine();
+
+        Console.Write($"{Table}{ColumnSeperaor}|   ");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            
+            Console.Write($"{i*Table}\t");
+
+        }
+    }
+
+    static void PrintMultiplicationTables()
+    {
+        for(int i = 1;i <= 10;i++)
+        {
+            if (i == 10)
+            {
+                PrintMultiPlicationTableX(i, "   ");
+                break;
+            }
+               
+
+            PrintMultiPlicationTableX (i);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        PrintHeader();
+        PrintMultiplicationTables();
+        Console.ReadKey();
+    }
+}
