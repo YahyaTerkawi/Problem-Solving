b07db0a70357f9d0076343fbd0ed53ba00fc7a0a
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-14 11:55:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+
+    static void PrintInvertedLetterPattern(int num)
+    {
+        for (int i = 64+num; i > 64; i--)
+        {
+            for(int j = 64 ; j<i; j++)
+            {
+                Console.Write((char)i);
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main(string[] args)
+    {
+        PrintInvertedLetterPattern(9);
+    }
+}
