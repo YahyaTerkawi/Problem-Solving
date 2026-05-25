d0451cff0f51b2a16c91635b8804bf91e3e6d1c7
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-25 10:27:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class program
+{ 
+
+    static void PrintAllWordsFromAAAToZZZ()
+    {
+        for(int i = 65; i<=90; i++)
+        {
+            for(int j = 65; j<= 90; j++)
+            {
+                for( int k = 65; k<= 90;  k++)
+                {
+                    Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                }
+            }
+        }
+    }
+
+    static void PrintAllWordsFromAAAToZZZ2()
+    {
+        for (char i = 'A'; i <= 'Z'; i++)
+        {
+            for (char j = 'A'; j <= 'Z'; j++)
+            {
+                for (char k = 'A'; k <= 'Z'; k++)
+                {
+                    Console.WriteLine($"{i}{j}{k}");
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        PrintAllWordsFromAAAToZZZ2();
+    }
+}
