87b26fa946bd8ba7668bc94c964c1f1950295f02
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-25 10:10:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class program
+{
+
+    static void PrintLetterPattern(int num)
+    {
+        for (int i = 65; i < 65 + num; i++)
+        {
+            for (int j = 65; j <= i; j++)
+            {
+                Console.Write((char)i);
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        PrintLetterPattern(9);
+    }
+}
